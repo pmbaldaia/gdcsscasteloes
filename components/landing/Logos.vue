0437468132f4762d68d4e26edbebdc2ff2725dc0
Pedro Baldaia <pmbaldaia@NULL.Home>
2025-06-09 14:53:52 +0100

    adição patrocinadores

--- components/landing/Logos.vue
+++ components/landing/Logos.vue
@@ -5,10 +5,43 @@ const sponsors = [
   { src: "/patrocinios/fronteiratenciosa.png", alt: "FronteiraTenciosa" },
   { src: "/patrocinios/casacarreira.jpeg", alt: "Casa da Carreira" },
   { src: "/patrocinios/carvalhaparque.png", alt: "Carvalha Parque" },
-  { src: "/patrocinios/fontevelha.png", alt: "Fonte Velha" },
+  { src: "/patrocinios/fontevelha.JPEG", alt: "Fonte Velha" },
+  { src: "/patrocinios/allianz.jpeg", alt: "Allianz" },
+  { src: "/patrocinios/amstransportes.jpeg", alt: "AMS Transportes" },
+  { src: "/patrocinios/bjconstrucoes.jpeg", alt: "BJ Construções" },
+  { src: "/patrocinios/carlosmorango.jpeg", alt: "Carlos Morango" },
+  { src: "/patrocinios/cmcar.jpeg", alt: "CM Car" },
   { src: "/patrocinios/construcao.png", alt: "Construção" },
-  { src: "/patrocinios/luis moreira.jpg", alt: "Muis Moreira" },
+  { src: "/patrocinios/farmaciamoreira.jpeg", alt: "Farmácia Moreira" },
+  { src: "/patrocinios/fribeiro.jpeg", alt: "Fribeiro" },
+  { src: "/patrocinios/idmconstruct.jpeg", alt: "IDM Construct" },
+  { src: "/patrocinios/iluminacoescampos.jpeg", alt: "Iluminações Campos" },
+  { src: "/patrocinios/jardeco.jpeg", alt: "Jardeco" },
+  { src: "/patrocinios/luis moreira.jpg", alt: "Luís Moreira" },
+  { src: "/patrocinios/mocacar.jpeg", alt: "Mocacar" },
+  { src: "/patrocinios/nortenha.jpeg", alt: "Nortenha" },
+  { src: "/patrocinios/oliveIrapadaria.jpeg", alt: "Oliveira Padaria" },
+  { src: "/patrocinios/pardais.jpeg", alt: "Pardais" },
+  { src: "/patrocinios/piscinaspaulo.jpeg", alt: "Piscinas Paulo" },
+  { src: "/patrocinios/pizzariamoderna.jpeg", alt: "Pizzaria Moderna" },
+  { src: "/patrocinios/pneusribeiro.jpeg", alt: "Pneus Ribeiro" },
+  { src: "/patrocinios/quintaagualeveda.jpeg", alt: "Quinta Água Levada" },
+  { src: "/patrocinios/realcafes.jpeg", alt: "Real Cafés" },
+  {
+    src: "/patrocinios/restauranterecesinhos.jpeg",
+    alt: "Restaurante e Receisinhos",
+  },
+  { src: "/patrocinios/rimaquinas.jpeg", alt: "Rimaquinas" },
+  { src: "/patrocinios/sigaseguro.jpeg", alt: "SigaSeguro" },
   { src: "/patrocinios/sofas.png", alt: "Sofas" },
+  { src: "/patrocinios/splcar.jpeg", alt: "SPL Car" },
+  { src: "/patrocinios/steelferreiro.jpeg", alt: "Steel Ferreiro" },
+  { src: "/patrocinios/tabernacruz.jpeg", alt: "Taberna Cruz" },
+  { src: "/patrocinios/tamega.jpeg", alt: "Tâmega" },
+  { src: "/patrocinios/tiagomoreira.jpeg", alt: "Tiago Moreira" },
+  { src: "/patrocinios/triunfoclassico.jpeg", alt: "Triunfo Clássico" },
+  { src: "/patrocinios/vataxi.jpeg", alt: "VA Táxi" },
+  { src: "/patrocinios/zecananda.jpeg", alt: "Zé Cananda" },
 ];
 </script>
 
@@ -19,32 +52,14 @@ const sponsors = [
     >
       Patrocínios
     </h2>
-    <div
-      class="flex flex-wrap gap-8 md:gap-20 items-center justify-center mt-10"
-    >
+    <div class="grid grid-cols-2 md:grid-cols-5 gap-8 md:gap-10 mt-10 px-4">
       <img
         v-for="(sponsor, i) in sponsors"
         :key="i"
         :src="sponsor.src"
         :alt="sponsor.alt"
-        :class="[
-          'object-contain',
-          sponsor.alt === 'CFDS'
-            ? 'h-[120px] max-w-[160px] bg-black p-2 dark:bg-transparent'
-            : sponsor.alt === 'M Pinto'
-            ? 'h-[180px] max-w-[220px]'
-            : 'h-[120px] max-w-[160px]',
-        ]"
+        class="object-contain h-[120px] w-full max-w-[160px] mx-auto"
       />
-
-      <div
-        class="h-[120px] max-w-[160px] flex flex-col justify-center items-center rounded-lg border-2 border-dashed border-gray-300 dark:border-gray-600 bg-gray-50 dark:bg-gray-800 text-gray-700 dark:text-gray-300 font-semibold text-center px-6 shadow-md cursor-default select-none"
-      >
-        <span class="text-5xl mb-1 font-extrabold leading-none">+</span>
-        <span class="text-base leading-tight pb-6">
-          Mais patrocínios<br />em breve
-        </span>
-      </div>
     </div>
   </section>
 </template>
